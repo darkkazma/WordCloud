0c11cf6692a2f65a93eb5a60b8f84ad2c359f8c8
unknown <darkkazma@nate.com>
2020-07-14 20:30:09 +0900

    router추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import AppShell from './AppShell';
+import Home from './Home';
+import Texts from './Texts';
+import Words from './Words';
+
+
 
 class App extends React.Component{
     render(){
         return(
-            <div>
-                <h3>Hello World</h3>
-            </div>
+            <Router>
+                <AppShell>
+                <div>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/texts" component={Texts} />
+                    <Route exact path="/words" component={Words} />
+                </div>
+                </AppShell>
+            </Router>
         )
     }
 }
